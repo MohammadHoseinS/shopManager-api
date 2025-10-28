b7dacfc5b9b0095685bbb15a4f1b8b85197297bc
MohammadHoseinS <mohammadhosein.solomon@gmail.com>
2025-10-28 22:37:26 +0330

    Add order status enum

---
+++ src/common/enums/order.status.ts
@@ -0,0 +1,6 @@
+// In a production application this should be moved to a common library (usabale by the front ent as well)
+export enum OrderStatus {
+	Draft = 'draft',
+	Finalized = 'finalized',
+	Canceled = 'canceled'
+}
